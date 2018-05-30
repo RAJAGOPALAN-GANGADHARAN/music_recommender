27e9c594e655e2de175cb4f4ff859e8d3d068aae
bgadberr <bgadberr@users.noreply.github.com>
2018-05-29 20:58:11 -0400

    cleanup

--- get_lastfm.py
+++ get_lastfm.py
@@ -2,45 +2,59 @@ import gzip
 from scipy.sparse import coo_matrix
 
 def get_lastfm(min_plays=200):
-    # coo_matrix
+
+    # [plays], [user index], [artist index]
     data, row, col = [], [], []
 
-    artists, users = {}, {}
+    # {'artist_id': {'id': index, 'name': 'artist_name'}}
+    artists = {}
 
+    # {'user_id': index}
+    users = {}
+
+    # open data set
     musicFile = gzip.open('/Users/brettgadberry/Desktop/lastfm-dataset-360K.tar.gz', 'rt')
-
+    
+    # skip header
     iter_musicFile = iter(musicFile)
     next(iter_musicFile)
 
+    # read data set
     for n, line in enumerate(iter_musicFile):
         readable_data  = line.split('\t')
-        user = readable_data[0]
+        user_id = readable_data[0]
         artist_id = readable_data[1]
         artist_name = readable_data[2]
         plays = int(readable_data[3])
         
-        if user not in users:
-            users[user] = len(users)
-
+        # add user to users dictionary 
+        if user_id not in users:
+            users[user_id] = len(users)
+        
+        # add artist to artists dictionary 
         if artist_id not in artists:
             artists[artist_id] = {
                 'name' : artist_name,
                 'id' : len(artists)
                 }
-
+        
+        # add data to coo_matrix
         if plays > min_plays:
             data.append(plays)
-            row.append(users[user])
+            row.append(users[user_id])
             col.append(artists[artist_id]['id'])
         
+        # end function at n rows
         if n == 100000: break
 
+    # complie matrix
     coo = coo_matrix((data,(row,col)))
 
+    # return matrix, artists dictionary, amount of users
     dictionary = {
         'matrix' : coo,
         'artists' : artists,
         'users' : len(users)
         }
-    
+
     return dictionary
--- music_recommender.py
+++ music_recommender.py
@@ -2,33 +2,29 @@ import numpy as np
 from lightfm import LightFM
 from get_lastfm import get_lastfm
 
-
+# get final data set from get_lastfm
 data = get_lastfm()
 
 model = LightFM(loss='warp')
 model.fit(data['matrix'], epochs=30, num_threads=2)
 
-def get_recommendations(model, coo_mtrx, users_ids):
+def get_recommendations(model, coo, users):
 
-    n_items = coo_mtrx.shape[1]
+    n = coo.shape[1]
 
-    for user in users_ids:
-        scores = model.predict(user, np.arange(n_items))
+    for user in users:
+        scores = model.predict(user, np.arange(n))
         top_scores = np.argsort(-scores)[:3]
 
         print('Recomendations for user %s:' % user)
 
-        for x in top_scores.tolist():
-            for artist, values in data['artists'].items():
-                if int(x) == values['id']:
+        for score in top_scores.tolist():
+            for artist_id, values in data['artists'].items():
+                if int(score) == values['id']:
                     print('   - %s' % values['name'])
 
-        print('\n') 
-
-
-user_1 = input('Select user_1 (0 to %s): ' % data['users'])
-user_2 = input('Select user_2 (0 to %s): ' % data['users'])
-user_3 = input('Select user_3 (0 to %s): ' % data['users'])
-print('\n') 
+user_1 = input('Select user 1 (0 to %s): ' % data['users'])
+user_2 = input('Select user 2 (0 to %s): ' % data['users'])
+user_3 = input('Select user 3 (0 to %s): ' % data['users'])
 
 get_recommendations(model, data['matrix'], [user_1, user_2, user_3])
