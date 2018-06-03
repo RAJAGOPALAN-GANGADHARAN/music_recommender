467aa0d56c0dc54ab66439397ed7fec548f1c45e
bgadberr <bgadberr@users.noreply.github.com>
2018-06-03 14:39:59 -0400

    cleanup

--- music_recommender.py
+++ music_recommender.py
@@ -1,6 +1,7 @@
 import numpy as np
 from lightfm import LightFM
 from get_lastfm import get_lastfm, my_artists
+import sys
 
 # get data matrix, artists, and users from get_lastfm
 data = get_lastfm()
@@ -12,28 +13,24 @@ model.fit(data['matrix'], epochs=30, num_threads=2)
 # get recommendations from model 
 def get_recommendations(model, coo, user):
 
-    # makes prediction based on end user values
+    # makes prediction based on end user valuesdr. dog
     n = coo.shape[1]
     try: scores = model.predict(user, np.arange(n))
     except ValueError:
         print('entered artists not found')
+        sys.exit()
     
     # get top 10 recommendations
-    top_scores = np.argsort(-scores)[:10]
-
-    # determine top 5 recommendations
     top_recommendations = []
-    for score in top_scores.tolist():
-        if len(top_recommendations) == 5: break
-        for artist_id, values in data['artists'].items():
-            if int(score) == values['id']:
-                if values['name'] == 'rock universal': break
-                elif values['name'] == my_artists[0]: break
-                elif values['name'] == my_artists[1]: break
-                elif values['name'] == my_artists[2]: break
-                else: 
-                    top_recommendations.append(values['name'])
-                    break
+    values = list(data['artists'].values())
+    top_scores = np.argsort(-scores)[:10]
+    for score in top_scores:
+        recommendation = values[score]['name']
+        if recommendation == 'rock universal': continue
+        elif recommendation == my_artists[0]: continue
+        elif recommendation == my_artists[1]: continue
+        elif recommendation == my_artists[2]: continue
+        else: top_recommendations.append(recommendation)
     
     # disply top 5 recommendations 
     print('Your music recommedations are:')
