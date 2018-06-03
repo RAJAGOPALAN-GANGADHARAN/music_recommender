215db49c193de81e34e481507035eec93ac1859b
bgadberr <bgadberr@users.noreply.github.com>
2018-06-02 20:12:33 -0400

    cleanup

--- music_recommender.py
+++ music_recommender.py
@@ -12,21 +12,33 @@ model.fit(data['matrix'], epochs=30, num_threads=2)
 # get recommendations from model 
 def get_recommendations(model, coo, user):
 
+    # makes prediction based on end user values
     n = coo.shape[1]
-
     try: scores = model.predict(user, np.arange(n))
     except ValueError:
         print('entered artists not found')
+    
+    # get top 10 recommendations
     top_scores = np.argsort(-scores)[:10]
 
-    print('Your music recommedations are:')
-
+    # determine top 5 recommendations
+    top_recommendations = []
     for score in top_scores.tolist():
+        if len(top_recommendations) == 5: break
         for artist_id, values in data['artists'].items():
             if int(score) == values['id']:
-                if values['name'] == 'rock universal': continue
-                for artist in my_artists:
-                    if values['name'] == artist: continue
-                print(' - %s' % values['name'])
+                if values['name'] == 'rock universal': break
+                elif values['name'] == my_artists[0]: break
+                elif values['name'] == my_artists[1]: break
+                elif values['name'] == my_artists[2]: break
+                else: 
+                    top_recommendations.append(values['name'])
+                    break
+    
+    # disply top 5 recommendations 
+    print('Your music recommedations are:')
+    for i in range(5):
+        print(' - %s' % top_recommendations[i])
 
+# get recommendations from model
 get_recommendations(model, data['matrix'], data['users'])
